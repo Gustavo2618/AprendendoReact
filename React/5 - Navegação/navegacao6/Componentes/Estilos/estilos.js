a132f7837cb1ac4069be4c09c9c5df58cdb0ee02
Gustavo2618 <luizg473@gmail.com>
2022-11-17 13:07:32 -0300

    Concertando conflito

--- React/5 - Navegação/navegacao6/Componentes/Estilos/estilos.js
+++ React/5 - Navegação/navegacao6/Componentes/Estilos/estilos.js
@@ -3,16 +3,9 @@ import { StyleSheet } from "react-native";
 const estilos = StyleSheet.create({
     container:{
         flex:1,
-<<<<<<< HEAD
-        alignItems:'center',
-        justifyContent:'center'
-       
-=======
         alignContent:'center',
         alignItems:'center',
         justifyContent:'center',
-        
->>>>>>> c31a7491fd2070ef23d05146c2cd833bc508f31e
     },   
     texto:{
         alignItems:'center',
