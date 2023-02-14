c859d775ac25eae5cf5333a3e1b0332c6753254c
Luiz Gustavo <luizg473@gmail.com>
2023-02-14 11:50:02 -0300

    Add tela Sobre

---
+++ React/Portifolio/portifolio1/componentes/TelaSobre/estilos.js
@@ -0,0 +1,46 @@
+import { StyleSheet } from "react-native";
+
+const estilos = StyleSheet.create(
+    {
+        container:{
+            flex:1,
+            padding:10,
+            alignItems:"center",
+            justifyContent:"center",
+            backgroundColor:'#209000',
+        },
+        card:{
+            backgroundColor:'#fff',
+            borderRadius:20,
+            padding:10,
+            alignItems:'center',
+            borderWidth:5,
+        },
+        titulo:{
+            fontSize:20,
+            fontWeight:"bold",
+            marginBottom:5
+        },
+        descricao:{
+            fontSize:16,
+            marginBottom:5,
+        },
+        contato:{
+            fontSize:16,
+            marginBottom:5,
+            fontWeight:"bold"
+        },
+        img:{
+            width:100,
+            height:100,
+            borderRadius:100,
+            
+
+        },
+        
+    }
+)
+
+
+
+export default estilos;
--- React/Portifolio/portifolio1/componentes/TelaSobre/index.js
+++ React/Portifolio/portifolio1/componentes/TelaSobre/index.js
@@ -1,13 +1,24 @@
 import React from "react";
-import { View, Image, Button, Text } from 'react-native';
+import { View, Image, Text } from 'react-native';
+import estilos from "./estilos";
+import sobre from '../../assets/perfil.jpeg';
+
 
 
 
 
 export default function TelaSobre() {
     return (
-        <View>
-            <Text> TelaSobre</Text>
+        <View style={estilos.container}>
+            <View style={estilos.card}>
+                <Image source={sobre} style={estilos.img}></Image>
+                <Text style={estilos.titulo}> Luiz Gustavo Gomes Monteiro</Text>
+                <Text style={estilos.descricao}>Aluno do curso de Bacharelado em Ciência da Computação na Universidade Estadual do Ceará</Text>
+                <Text style={estilos.contato}>Idade: 31</Text>
+                <Text style={estilos.contato}>Tel: (85) 989454373</Text>
+                <Text style={estilos.contato}>Email: luizg473@gmail.com</Text>
+
+            </View>
         </View>
     )
 };
